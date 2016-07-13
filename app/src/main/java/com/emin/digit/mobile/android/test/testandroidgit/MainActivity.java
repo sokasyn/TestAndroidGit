a0611bdf8f800e60b6c69a255126f90bd7c5c073
sokasyn <sokasyn@qq.com>
2016-07-13 17:58:12 +0800

    MainActivity modified

--- app/src/main/java/com/emin/digit/mobile/android/test/testandroidgit/MainActivity.java
+++ app/src/main/java/com/emin/digit/mobile/android/test/testandroidgit/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     private View.OnClickListener mClickListener = new View.OnClickListener() {
