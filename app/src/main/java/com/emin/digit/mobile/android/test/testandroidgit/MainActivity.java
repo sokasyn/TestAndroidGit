7b54f422eec957babab66b4964dcc92b02c508f5
sokasyn <sokasyn@qq.com>
2016-07-13 17:53:10 +0800

    Project created

---
+++ app/src/main/java/com/emin/digit/mobile/android/test/testandroidgit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.emin.digit.mobile.android.test.testandroidgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
